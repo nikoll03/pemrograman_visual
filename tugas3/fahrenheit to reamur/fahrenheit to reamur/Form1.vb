f21b42b5497a0f8d029dd78b20cdf621788845b1
nikoll03 <128014286+nikoll03@users.noreply.github.com>
2023-05-03 22:17:19 +0700

    Add files via upload

---
+++ tugas3/fahrenheit to reamur/fahrenheit to reamur/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Function KonversiFahrenheittoReamur(f As Integer) As Integer
+        Dim r As Integer
+        r = (4 / 9) * (f - 32)
+        Return r
+    End Function
+    Private Sub btnkonversi_Click(sender As Object, e As EventArgs) Handles btnkonversi.Click
+        Dim f, r As Integer
+
+        f = Val(txtfahre.Text)
+
+        r = KonversiFahrenheittoReamur(f)
+        txtreamur.Text = Str(r)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
