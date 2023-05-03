21655c9ebffa6ae71221a81640545ad7bcacda15
nikoll03 <128014286+nikoll03@users.noreply.github.com>
2023-05-03 21:58:42 +0700

    Add files via upload

---
+++ latihan3/Lingkaran/Lingkaran/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub btnhitung_Click(sender As Object, e As EventArgs) Handles btnhitung.Click
+        Dim jari, luas, keliling As Integer
+
+        jari = Val(txtjari.Text)
+
+        luas = 22 / 7 * (jari * jari)
+        keliling = 2 * 22 / 7 * jari
+
+        txtluas.Text = Str(luas)
+        txtkeliling.Text = Str(keliling)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
